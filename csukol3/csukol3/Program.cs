74556c9f9c34456bcdcb50561777ee7df2561daa
jakubsps6 <144690916+jakubsps6@users.noreply.github.com>
2024-04-23 16:56:27 +0200

    NEDODĚLÁNO

---
+++ csukol3/csukol3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csukol3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            Console.WriteLine("Zadejte prikaz");
+            string prikaz = Console.ReadLine();
+            Console.WriteLine("Zadejte hodnotu");
+            string a = Console.ReadLine();
+            if (prikaz == "pridat")
+            {
+                queue.Enqueue(a);
+            } else if (prikaz == )
+        }
+    }
+}
